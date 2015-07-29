974ecd5cba1ff481c0d1b5fad2289d721b664cd4
mmilian <mateusz.milian@gmail.com>
2015-07-29 14:24:16 +0200

    Added support for obywatele

--- run.js
+++ run.js
@@ -1,22 +1,23 @@
 var http = require('http');
 var jsreport = require('jsreport');
 var fs = require('fs');
-var request = require('request'),
-    url = "https://pelnomocnicy.apispark.net/v1/pelnomocnicies?$size=1000";
+var urler = require('url');
+var request = require('request');
 //https://037fc8c9-9375-46e5-a209-e19808257c38:0a3910ad-8070-4f55-8a88-6004bf507d9c" + 
 
 var port = Number(process.env.PORT || 8888);
 
 http.createServer(function (req, res) {
-  
-  console.log(req.url);
+var params = urler.parse(req.url, true).query;
+var template = (params.inst == "obywatele") ? "templateObywatele.html" :"templateJOW.html";
+var url = ((params.file ==  "old") ? "https://pelnomocnicy.apispark.net/v1/pelnomocnicies?$size=1000" : "https://pelnomocnicy.apispark.net/v1/pelnomocnicies?$size=1000");
   request(
     {
         url : url
     },
     function (error, response, body) {
           console.log(JSON.parse(body).length);
-          var contents = fs.readFileSync('template.html', 'utf8');
+          var contents = fs.readFileSync(template, 'utf8');
           jsreport.render({
           	 template: {
  			  content:contents,
